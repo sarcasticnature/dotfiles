cb4859b3d2457aeeba2a0765b0728945255ead15
jkeller <jkeller@nauticusrobotics.com>
2022-01-21 19:35:23 -0600

    Fix missed stuff in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,15 @@
 set -e
 
 # install zsh
-apt update && apt install -y vim tmux zsh git
+apt update && apt install -y vim tmux zsh git curl fonts-powerline
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattanded
 
 ZSH_CUSTOM=~/.oh-my-zsh
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+cp .zshrc ~
 
 # download/install vim-plug
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
