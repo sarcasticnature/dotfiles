73aed7c65f3f08012738550a4c48065ff6981311
jkeller <jkeller@nauticusrobotics.com>
2022-01-21 18:32:04 -0600

    Begin updating setup.sh to actually be useful

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e
+
+# install zsh
+sudo apt update && sudo apt install -y vim tmux zsh git
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 # download/install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
