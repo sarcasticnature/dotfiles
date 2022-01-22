c3c71e3046060362c8227ece0fc2265e2e4b7983
jkeller <jkeller@nauticusrobotics.com>
2022-01-21 19:26:47 -0600

    Fixes for oh-my-zsh install script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@
 set -e
 
 # install zsh
-sudo apt update && sudo apt install -y vim tmux zsh git
+apt update && apt install -y vim tmux zsh git
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattanded
 
+ZSH_CUSTOM=~/.oh-my-zsh
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
