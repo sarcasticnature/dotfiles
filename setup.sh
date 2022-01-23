4644cce5cea6f495022b61adc6e4afde2ff480c9
Jake Keller <jakeller3630@gmail.com>
2022-01-22 22:04:48 -0600

    Change to the Starship prompt

--- setup.sh
+++ setup.sh
@@ -6,9 +6,10 @@ apt update && apt install -y vim tmux zsh git curl fonts-powerline
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattanded
 
-ZSH_CUSTOM=~/.oh-my-zsh
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
+#ZSH_CUSTOM=~/.oh-my-zsh
+#git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+#ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme
 
 cp .zshrc ~
 
