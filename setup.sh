5bb5e2e2c9f40eb434d291506754ed055bc11077
Jake Keller <jakeller3630@gmail.com>
2022-01-22 22:10:53 -0600

    Copy starship.toml over in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ apt update && apt install -y vim tmux zsh git curl fonts-powerline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattanded
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --yes
+mkdir -p ~/.config
+cp starship.toml ~/.config
+
 #ZSH_CUSTOM=~/.oh-my-zsh
 #git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 #ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme
