d567f2b60a46f4c969a043bd28443069782228da
Jake Keller <jakeller3630@gmail.com>
2024-07-03 19:58:16 +0000

    Updates

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 apt update && apt install -y clang-tidy
-cp -r /root/volume/Git/dotfiles/.vim* /root
-cp -r /root/volume/Git/dotfiles/.config/nvim /root/.config/
-mkdir -p ~/.vim/pack/git-plugins/start
-git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+cp -r .vim* /root
+mkdir -p /root/.config
+cp -r .config/nvim /root/.config/
+cp -r .config/starship.toml /root/.config/
+mkdir -p ~/.local/share/nvim/site/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.local/share/nvim/site/pack/git-plugins/start/ale
 
-echo "stty -ixon" >> ~/.bashrc.d/100.bash_prompt.sh
-echo "export VISUAL=/usr/bin/vim" >> ~/.bashrc.d/100.bash_prompt.sh
+echo "stty -ixon" >> ~/.bashrc
+echo "export VISUAL=/usr/bin/vim" >> ~/.bashrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
