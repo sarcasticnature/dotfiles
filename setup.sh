3dce866deeded1a6c591fffee889e44b05903412
Jake Keller <jakeller3630@gmail.com>
2025-11-03 08:16:08 -0500

    Update setup.sh to use home dir

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-apt update && apt install -y clang-tidy
-cp -r .vim* /root
-mkdir -p /root/.config
-cp -r .config/nvim /root/.config/
-cp -r .config/starship.toml /root/.config/
+#apt update && apt install -y clang-tidy
+cp -r .vim* $HOME
+mkdir -p $HOME/.config
+cp -r .config/nvim $HOME/.config/
+cp -r .config/starship.toml $HOME/.config/
 mkdir -p ~/.local/share/nvim/site/pack/git-plugins/start
 git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.local/share/nvim/site/pack/git-plugins/start/ale
 
-echo "stty -ixon" >> ~/.bashrc
+#echo "stty -ixon" >> ~/.bashrc
 echo "export VISUAL=/usr/bin/vim" >> ~/.bashrc
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
