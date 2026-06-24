a94c92105105d1387686632e1401f0fea53c0968
Jake Keller <jakeller3630@gmail.com>
2026-06-23 21:43:04 -0400

    Update treesitter && lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,7 @@ return {
 
         -- Allows extra capabilities provided by blink.cmp
         "saghen/blink.cmp",
+        "saghen/blink.lib",
     },
     config = function()
         -- Brief aside: **What is LSP?**
@@ -168,7 +169,19 @@ return {
         --  - settings (table): Override the default settings passed when initializing the server.
         --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
-            -- clangd = {},
+            clangd = {
+                cmd = {
+                    "clangd",
+                    "--query-driver=/usr/bin/gcc,/usr/bin/g++",
+                    --"--compile-commands-dir=/home/jake/workspaces/template/build",
+                },
+                --capabilities = {
+                --    arguments = {
+                --        "--query-driver=/usr/bin/gcc,/usr/bin/g++",
+                --        "--compile-commands-dir=/home/jake/workspaces/template/build",
+                --    },
+                --},
+            },
             -- gopls = {},
             -- pyright = {},
             -- rust_analyzer = {},
@@ -197,6 +210,11 @@ return {
             },
         }
 
+        for server, config in pairs(servers) do
+            vim.lsp.config[server] = config
+            vim.lsp.enable(server)
+        end
+
         -- Ensure the servers and tools above are installed
         --
         -- To check the current status of installed tools and/or manually install
@@ -216,22 +234,24 @@ return {
             "hls",
             "hlint",
             "ormolu",
+            --"clangd",
         })
         require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
         require("mason-lspconfig").setup({
             ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
             automatic_installation = false,
-            handlers = {
-                function(server_name)
-                    local server = servers[server_name] or {}
-                    -- This handles overriding only values explicitly passed
-                    -- by the server configuration above. Useful when disabling
-                    -- certain features of an LSP (for example, turning off formatting for ts_ls)
-                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-                    require("lspconfig")[server_name].setup(server)
-                end,
-            },
+            --handlers = {
+            --    function(server_name)
+            --        local server = servers[server_name] or {}
+            --        -- This handles overriding only values explicitly passed
+            --        -- by the server configuration above. Useful when disabling
+            --        -- certain features of an LSP (for example, turning off formatting for ts_ls)
+            --        server.capabilities =
+            --            vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            --        require("lspconfig")[server_name].setup(server)
+            --    end,
+            --},
         })
     end,
 }
